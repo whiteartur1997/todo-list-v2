7409f69c77b3d6362f6600d77da290bbe24014c4
whiteartur1997 <whiteartur1997@gmail.com>
2020-08-12 16:25:08 +0300

    Updates

---
+++ precache-manifest.02b0d4e033447dd0372cc500699db2ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29a1691820e3710d1a5bbdf2049844e",
+    "url": "/todo-list-v2/index.html"
+  },
+  {
+    "revision": "4382726668b00059e42c",
+    "url": "/todo-list-v2/static/css/main.4f224e88.chunk.css"
+  },
+  {
+    "revision": "bbd1fab97d789ab62903",
+    "url": "/todo-list-v2/static/js/2.10ae5075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list-v2/static/js/2.10ae5075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4382726668b00059e42c",
+    "url": "/todo-list-v2/static/js/main.76db1de1.chunk.js"
+  },
+  {
+    "revision": "2b89bc8b0d72152ee829",
+    "url": "/todo-list-v2/static/js/runtime-main.c9206649.js"
+  }
+]);
